7347bac6573f958efcd9a6a00a6dc699ece9d88e
Ahmad Dwi Setiawan <ahmaddwisetiawan045@gmail.com>
2026-05-11 15:17:50 +0000

    speed up build

--- install.sh
+++ install.sh
@@ -9,14 +9,16 @@ mkdir -p bootstrap/cache \
 chown -R www-data:www-data bootstrap storage || true
 chmod -R ug+rwx bootstrap storage || true
 
-npm install --legacy-peer-deps --no-audit --progress=false
-npm run dev
+# npm install --legacy-peer-deps --no-audit --progress=false
+# npm run dev
+
 composer install --optimize-autoloader
+
 cp .env.example .env || true
 php artisan key:generate
 
 sed -i 's/DB_HOST=127.0.0.1/DB_HOST=172.17.0.2/g' .env
 sed -i 's/DB_PASSWORD=/DB_PASSWORD=password/g' .env
 
-php artisan migrate --force
-php artisan db:seed --force
+# php artisan migrate --force
+# php artisan db:seed --force
